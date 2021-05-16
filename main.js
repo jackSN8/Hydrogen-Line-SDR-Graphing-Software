4683cee7f344a0816c84d63d03720c62a732f4b0
jackSN8 <jackin0708@highgateschool.org.uk>
2021-05-16 14:51:16 +0100

    Graph kinda done. Worst scale ever.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let inpStr=[];
 let totalFiles = 1;
 let frequencyArray = [];
 let strengthArray = [];
-
+const xAxisLabels=7;
 const hLinePhoton = 1420.4057517667;//MHZ
 const lightSpeed = 299792458;//MS^-1
 
@@ -28,7 +28,7 @@ function setup()
 
 function draw()
 {
-  background(255);
+  background(51, 51, 153);
   drawGraph(frequencyArray[0],strengthArray[0]);
 }
 
@@ -63,17 +63,31 @@ function drawGraph(inpArrayF,inpArrayS)
   {
     normalizedFreqs.push(map(inpArrayF[i],1418.9,1421.31,xDif,width-xDif));
     normalizedStregs.push(map(inpArrayS[i],0,0.1,height-yDif,yDif));
-    stroke(0,255,0);
+    stroke(127);
     point(normalizedFreqs[i],normalizedStregs[i]);
   }
   stroke(0);
   line(xDif,height-yDif,width-xDif,height-yDif);
   line(xDif,height-yDif,xDif,yDif);
+  text("Signal strength against frequency with H-LINE spectrum", width/3,yDif/2);
   text("Velocity / km(s^-1)",width/2,height-yDif/2);
   text("Signal strength",xDif/8,height/2);
+  text("/ jDB",xDif/8+4,height/2+14);
   //console.log(freqVelocities[511]);
 
-  text(freqVelocities[400],xDif+50,height-yDif/1.2);
+  ///Now label the x axis (velocity)
+  let xAxisLength = width-xDif*2;
+  let labelPos = [];
+  let velocityLabels = [];
+  for(let i=0; i<xAxisLabels; i++)//4 labels for kms
+  {
+    labelPos.push(map(i,0,xAxisLabels,xAxisLength/10,xAxisLength));
+    velocityLabels.push(map(i,0,xAxisLabels,freqVelocities.length/10,freqVelocities.length));
+    velocityLabels = int(velocityLabels);
+    //Creates 4 labels along the axis axis
+    text(int(freqVelocities[velocityLabels[i]]),labelPos[i]+xDif-11,height-yDif/1.2);
+    line(labelPos[i]+xDif,height-yDif,labelPos[i]+xDif,height-yDif-6);
+  }
 }
 
 function findVelocities(inpArrayF)
@@ -81,12 +95,12 @@ function findVelocities(inpArrayF)
   let velocities = [];
   for(let i=0; i<inpArrayF.length; i++)
   {
-    //Calculate doppler shift
-    let freqRatio = hLinePhoton/inpArrayF[i];
-    freqRatio = freqRatio*freqRatio;
-    let cRatio = freqRatio/(1+freqRatio);
-    let velocity = cRatio * lightSpeed;
+    //Calculate doppler shift using relativstic doppler equation (fs/fr) = sqrt((1+beta)/(1-beta))
+    let freqRatio = (hLinePhoton*hLinePhoton)/(inpArrayF[i]*inpArrayF[i]);
+    let beta = (freqRatio-1)/(freqRatio+1);
+    let velocity = beta * lightSpeed;
     //console.log(cRatio);
+    velocity *= 0.001;//convert to km/s
     velocities.push(velocity);
   }
   return velocities;
